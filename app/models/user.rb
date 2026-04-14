6724f71c4caaeb1549fb83e0d5ec3fc46c9539d7
KobiEau <nana.enyan-aidoo@st.vvu.edu.gh>
2026-04-14 15:15:30 +0000

    feat: add fullname helper to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,10 @@ class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
   has_many :contacts, dependent: :destroy
+  validates :firstname, presence: true
   normalizes :email_address, with: ->(e) { e.strip.downcase }
+
+  def fullname
+    "#{firstname} #{lastname}".strip
+  end
 end
