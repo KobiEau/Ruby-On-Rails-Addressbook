e3f8759ae91758863b553e16b10ef5329840f93d
KobiEau <nana.enyan-aidoo@st.vvu.edu.gh>
2026-04-14 15:04:54 +0000

    feat(db):add firstname and lastname and user_id field to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_13_165137) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_14_132640) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -20,6 +20,8 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_13_165137) do
     t.string "lastname"
     t.string "phone_number"
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_contacts_on_user_id"
   end
 
   create_table "sessions", force: :cascade do |t|
@@ -34,10 +36,13 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_13_165137) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email_address", null: false
+    t.string "firstname"
+    t.string "lastname"
     t.string "password_digest", null: false
     t.datetime "updated_at", null: false
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
 
+  add_foreign_key "contacts", "users"
   add_foreign_key "sessions", "users"
 end
