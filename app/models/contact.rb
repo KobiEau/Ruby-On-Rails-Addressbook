d2dabb59a60141cfe158a88525bcc82433bf99b9
KobiEau <nana.enyan-aidoo@st.vvu.edu.gh>
2026-04-15 10:25:02 +0000

    refactor: add numeric only validation to phone_number field

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,5 @@
 class Contact < ApplicationRecord
   belongs_to :user
   validates :firstname,:lastname, presence: true ,length: {minimum: 1}
-  validates :phone_number, presence: true
+  validates :phone_number, presence: true, numericality:{greater_than:0}
 end
