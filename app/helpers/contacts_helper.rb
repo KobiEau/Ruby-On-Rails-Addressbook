6ac5249b651c3ae2942f6c4fadc9f40d48067485
KobiEau <nana.enyan-aidoo@st.vvu.edu.gh>
2026-04-17 10:10:51 +0000

    feat: generate contact initials and background colour

--- app/helpers/contacts_helper.rb
+++ app/helpers/contacts_helper.rb
@@ -1,2 +1,12 @@
 module ContactsHelper
+  def contact_initials(contact)
+    "#{contact.firstname[0]}#{contact.lastname[0]}".upcase
+  end
+
+  def contact_initials_color(contact)
+    #trying for different colours based on initials
+    colours =["bg-cyan-500","bg-slate-500","bg-blue-500","bg-indingo-500","bg-green-500","bg-blue-500"]
+    index = (contact.firstname+contact.lastname).to_s.sum % colours.length
+    colours[index]
+  end
 end
