fe789525f7fbae221cdeda1b00c38e718bca294b
KobiEau <nana.enyan-aidoo@st.vvu.edu.gh>
2026-04-23 15:50:03 +0000

    feat: add phone_number validation

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,10 @@
 class Contact < ApplicationRecord
   belongs_to :user
   validates :firstname,:lastname, presence: true ,length: {minimum: 1}
-  validates :phone_number, presence: true, numericality:{greater_than:0}
+  validates :phone_number, format:{
+    with: /\A0[253]\d{8}\z/,
+    message: "must be 10-digit number starting with 02,03 or 05"
+  }
 
   def fullname
     [firstname,lastname].compact.join(" ")
