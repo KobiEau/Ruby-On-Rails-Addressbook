923361cdd7bd8c1fac236f9f5d43cf134113e728
KobiEau <nana.enyan-aidoo@st.vvu.edu.gh>
2026-04-23 15:43:03 +0000

    feat: add smtp and letter_opener mailer configs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,8 +32,25 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  # SMTP
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  # address: "smtp.gmail.com",
+  # port: 587,
+  # domain: "gmail.com",
+  # user_name: ENV["GMAIL_USERNAME"],
+  # password: ENV["GMAIL_APP_PASSWORD"],
+  # authentication: "plain",
+  # enable_starttls_auto: true
+  # }
+  # config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
 
+  # letter_opener
+  config.action_mailer.delivery method = :letter_opener
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: "localhost",port:3000}
+  
   # Make template changes take effect immediately.
   config.action_mailer.perform_caching = false
 
