40f907b845ef1b2b7489ddbe181a27157495c4c0
KobiEau <nana.enyan-aidoo@st.vvu.edu.gh>
2026-04-24 17:04:43 +0000

    feat: validate email uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :sessions, dependent: :destroy
   has_many :contacts, dependent: :destroy
   validates :firstname, presence: true
+  validates :email_address, presence: true, uniqueness: {case_sensitive:false}
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 
   def fullname
