3b8f03d289f7dbd8b0a6ebcfb0b860b655ade76a
KobiEau <nana.enyan-aidoo@st.vvu.edu.gh>
2026-04-24 17:13:39 +0000

    choere: update generated schema date to today

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_14_132640) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_24_142900) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
