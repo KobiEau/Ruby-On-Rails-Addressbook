37d8b28a91370f15c778a7c4470e2efafb6d4535
KobiEau <nana.enyan-aidoo@st.vvu.edu.gh>
2026-05-04 12:55:37 +0000

    chore: add category column to contacts

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_24_142900) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_04_103216) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   create_table "contacts", force: :cascade do |t|
+    t.string "category", default: "uncategorised"
     t.datetime "created_at", null: false
     t.string "firstname"
     t.string "lastname"
