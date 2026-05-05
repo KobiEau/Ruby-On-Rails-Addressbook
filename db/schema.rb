ca7a47bc80bc3e0db5e083b6fb2e4479f6bbdfad
KobiEau <nana.enyan-aidoo@st.vvu.edu.gh>
2026-05-05 16:10:50 +0000

    feat: add roles table, remove rails 8 auth provider-related colums, add devise-related columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_04_103216) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_05_002552) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -25,6 +25,12 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_04_103216) do
     t.index ["user_id"], name: "index_contacts_on_user_id"
   end
 
+  create_table "roles", primary_key: "code", id: :string, force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "name", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "sessions", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "ip_address"
@@ -36,14 +42,21 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_04_103216) do
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
-    t.string "email_address", null: false
+    t.string "email", null: false
+    t.string "encrypted_password", default: "", null: false
     t.string "firstname"
     t.string "lastname"
-    t.string "password_digest", null: false
+    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at"
+    t.string "reset_password_token"
+    t.string "role_code", default: "usr", null: false
     t.datetime "updated_at", null: false
-    t.index ["email_address"], name: "index_users_on_email_address", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["role_code"], name: "index_users_on_role_code"
   end
 
   add_foreign_key "contacts", "users"
   add_foreign_key "sessions", "users"
+  add_foreign_key "users", "roles", column: "role_code", primary_key: "code"
 end
