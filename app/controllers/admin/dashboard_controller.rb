51b7b6574e3c54b9c794e26b5bbfc05d44b052e9
KobiEau <nana.enyan-aidoo@st.vvu.edu.gh>
2026-05-06 16:13:25 +0000

    feat: add statistics for admin to view

---
+++ app/controllers/admin/dashboard_controller.rb
@@ -0,0 +1,8 @@
+class Admin::DashboardController < Admin::BaseController
+  def index
+    @total_users = User.count
+    @total_contacts = Contact.count
+    @admins = User.where(role_code: Role::ADMIN)
+    @recent_users = User.order(created_at: :desc).limit(5)
+  end
+end
