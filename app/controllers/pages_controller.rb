fec6ada4c0b885594f8b6bcb1c9959960ce58db0
KobiEau <nana.enyan-aidoo@st.vvu.edu.gh>
2026-05-06 16:09:55 +0000

    refactor: replace native Rails auth with devise pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,13 @@
 class PagesController < ApplicationController
-  skip_before_action :require_authentication
+  #skip_before_action :require_authentication
+  skip_before_action :authenticate_user!, only: [:home]
+  #skip Devise login requirement for the home page only
+  #Every other page requires login
   
   def home
-    redirect_to contacts_path if authenticated?
+    #redirect_to contacts_path if authenticated?
+    redirect_to contacts_path if user_signed_in?
+    # user_signed_in?- Devise helper
+    # if already logged in, skip homepage and go straight to contacts
   end
 end
