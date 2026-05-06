67cb8d4528081bc2ed7b66951d1a3ab087b894a0
KobiEau <nana.enyan-aidoo@st.vvu.edu.gh>
2026-05-06 16:11:27 +0000

    feat: add base class for admin auth

---
+++ app/controllers/admin/base_controller.rb
@@ -0,0 +1,5 @@
+class Admin::BaseController < ApplicationController
+  before_action :require_admin!
+  #controllers inheriting from this are admin-only
+  #guard is placed here instead of repeating in every other class
+end
