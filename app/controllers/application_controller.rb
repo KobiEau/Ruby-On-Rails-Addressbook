b587b4b2a18846c8ee81d2137f0fe3618697d034
KobiEau <nana.enyan-aidoo@st.vvu.edu.gh>
2026-05-08 10:20:18 +0000

    feat: add logic to redirect users based on role_code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,4 +36,12 @@ class ApplicationController < ActionController::Base
       redirect_to root_path
     end
   end
+
+  def after_sign_in_path_for(user)
+    user.admin? ? admin_root_path : contacts_path
+  end
+
+  def after_sign_up_path_for(user)
+    user.admin? ? admin_root_path : contacts_path
+  end
 end
