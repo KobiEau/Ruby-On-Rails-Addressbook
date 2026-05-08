61e59f2f19c10c93670b38dcf34f97819351bca7
KobiEau <nana.enyan-aidoo@st.vvu.edu.gh>
2026-05-08 10:21:30 +0000

    chore: add admin.html.erb reference

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,5 +1,6 @@
 class Admin::BaseController < ApplicationController
   before_action :require_admin!
+  layout "admin"
   #controllers inheriting from this are admin-only
   #guard is placed here instead of repeating in every other class
 end
