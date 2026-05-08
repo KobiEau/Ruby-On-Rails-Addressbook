6fc14d861ff2e4cbe5f1205b609d99726d3837b3
KobiEau <nana.enyan-aidoo@st.vvu.edu.gh>
2026-05-08 10:27:29 +0000

    fix: add '.count' method

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,7 +2,7 @@ class Admin::DashboardController < Admin::BaseController
   def index
     @total_users = User.count
     @total_contacts = Contact.count
-    @admins = User.where(role_code: Role::ADMIN)
+    @admins = User.where(role_code: Role::ADMIN).count
     @recent_users = User.order(created_at: :desc).limit(5)
   end
 end
