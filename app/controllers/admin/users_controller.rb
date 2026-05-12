e13452ad9b94ff5ff60e5a4d2ca5f60090e7a9df
KobiEau <nana.enyan-aidoo@st.vvu.edu.gh>
2026-05-12 12:37:19 +0000

    feat: add pagination and user creation in various methods

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,6 +2,15 @@ class Admin::UsersController < Admin::BaseController
   def index
     # includes(:role) — loads role data in same query,prevents N+1 query
     @users = User.includes(:role).order(created_at: :desc)
+
+    @per_page = (params[:per_page] || 10).to_i
+    @users = @users.where(
+      "firstanme ILIKE ? OR lastname ILIKE? OR email ILIKE?",
+      "%#{params[:search]}%","%#{params[:search]}%","%#{params[:search]}%"
+    )if params[:search].present?
+    
+    @users = @users.where(role_code: params[:role_filter]) if params[:role_filter].present?
+    @users = @users.page(params[:page]).per(@per_page)
   end
 
   def show
@@ -10,6 +19,20 @@ class Admin::UsersController < Admin::BaseController
 
   def edit
     @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(new_user_params)
+
+    if @user.save
+      redirect_to admin_users_path, notice:"User created successfully"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def new
+    @user = User.new
   end
 
   def update
@@ -31,5 +54,10 @@ class Admin::UsersController < Admin::BaseController
 
   def user_params
     params.expect(user: [:email, :role_code])
+  end
+
+  def new_user_params
+    params.expect(user:[:firstname, :lastname, :email,
+                  :password, :password_confirmation, :role_code])
   end
 end
