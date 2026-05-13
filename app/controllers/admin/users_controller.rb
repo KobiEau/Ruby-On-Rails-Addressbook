679e70e791bd7cb5565419dab032d584bf9d2b8e
KobiEau <nana.enyan-aidoo@st.vvu.edu.gh>
2026-05-13 11:50:09 +0000

    refactor: add cookie to remember user preference for number of paginated results

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,7 +3,8 @@ class Admin::UsersController < Admin::BaseController
     # includes(:role) — loads role data in same query,prevents N+1 query
     @users = User.includes(:role).order(created_at: :desc)
 
-    @per_page = (params[:per_page] || 10).to_i
+    @per_page = (params[:per_page] || cookies[:admin_users_per_page] || 10).to_i
+    cookies[:admin_users_per_page] = @per_page
     @users = @users.where(
       "firstanme ILIKE ? OR lastname ILIKE? OR email ILIKE?",
       "%#{params[:search]}%","%#{params[:search]}%","%#{params[:search]}%"
