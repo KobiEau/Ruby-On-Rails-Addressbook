aa07598b9762e7cc06377ff0e4e2a7121c15e727
KobiEau <nana.enyan-aidoo@st.vvu.edu.gh>
2026-05-18 09:59:18 +0000

    chore: add index to contact name,phone_number and user_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_05_002552) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_18_053024) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -22,6 +22,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_05_002552) do
     t.string "phone_number"
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.index ["firstname", "lastname", "phone_number", "user_id"], name: "index_contacts_on_name_and_phone_and_user", unique: true
     t.index ["user_id"], name: "index_contacts_on_user_id"
   end
 
