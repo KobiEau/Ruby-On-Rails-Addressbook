3e3b315f86d39ff334ee06c15e32686db6aae2fe
KobiEau <nana.enyan-aidoo@st.vvu.edu.gh>
2026-05-18 10:02:23 +0000

    feat: add scope on contact fields to prevent duplicates

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,7 +4,7 @@ class Contact < ApplicationRecord
   CATEGORIES = %w[Uncategorised Family Friends Work Colleagues].freeze
 
   validates :firstname,:lastname, presence: true ,length: {minimum: 1}
-  validates :phone_number, 
+  validates :phone_number, presence: true,
   format:{
     with: /\A0[253]\d{8}\z/,
     message: "must be 10-digit number starting with 02,03 or 05"
@@ -13,6 +13,12 @@ class Contact < ApplicationRecord
     #inclusion keyword validates only values from an accepted list
     in: CATEGORIES,
     message: "must be a valid category"
+  }
+
+  validates :firstname,
+  uniqueness:{
+    scope: [:lastname,:phone_number, :user_id],
+    message: "already has a contact with this name and number"
   }
 
   def fullname
