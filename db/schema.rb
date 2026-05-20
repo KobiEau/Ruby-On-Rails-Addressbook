a3558c94774c3aec3c45b33d7c430fa9a533259f
KobiEau <nana.enyan-aidoo@st.vvu.edu.gh>
2026-05-20 13:31:46 +0000

    feat(db): add current and last sign-in IP columns to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_19_162731) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_20_105202) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -44,11 +44,13 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_19_162731) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "current_sign_in_at"
+    t.string "current_sign_in_ip"
     t.string "email", null: false
     t.string "encrypted_password", default: "", null: false
     t.integer "failed_attempts"
     t.string "firstname"
     t.datetime "last_sign_in_at"
+    t.string "last_sign_in_ip"
     t.string "lastname"
     t.datetime "locked_at"
     t.datetime "remember_created_at"
