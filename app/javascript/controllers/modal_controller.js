1c18344b0049e4b5cc5078dc0679bd4957e81470
KobiEau <nana.enyan-aidoo@st.vvu.edu.gh>
2026-05-22 22:20:33 +0000

    feat: add password check before modal popup if it exists in a form

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -6,6 +6,21 @@ export default class extends Controller{
   open(event){
     event.preventDefault()
 
+    //if modal is tied to a form,check if current_password is filled
+    const formId = event.currentTarget.dataset.modalForm
+    if(formId){
+      const form= document.getElementById(formId)
+      const passwordField = form.querySelector("input[name*='current_password']")
+      if(passwordField && passwordField.value.trim() === ""){
+        passwordField.focus()
+        return
+    }}
+
+    this.messageTarget.textContent = event.currentTarget.dataset.modalMessage
+    this.confirmBtnTarget.dataset.url = event.currentTarget.dataset.modalUrl || ""
+    this.confirmBtnTarget.dataset.method = event.currentTarget.dataset.modalMethod || "delete"
+    this.confirmBtnTarget.dataset.form = event.currentTarget.dataset.modalForm || ""
+
     //Read message and action from clicked link
     const trigger = event.currentTarget
     this.messageTarget.textContent = trigger.dataset.modalMessage
@@ -22,17 +37,24 @@ export default class extends Controller{
   }
 
   confirm(){
-    const url = this.confirmBtnTarget.dataset.url
-    const method = this.confirmBtnTarget.dataset.method
+    const formId = this.confirmBtnTarget.dataset.form
 
-    //create a link - Turbo handles CSRF
-    const link = document.createElement("a")
-    link.href = url
-    link.dataset.turboMethod = method
-    document.body.appendChild(link)
-    link.click()
-    document.body.removeChild(link)
+    if(formId){
+      //password form path
+      document.getElementById(formId).submit()
+    }else{
+        //default path - create a link and click 
+      const url = this.confirmBtnTarget.dataset.url
+      const method = this.confirmBtnTarget.dataset.method
 
+      //create a link - Turbo handles CSRF
+      const link = document.createElement("a")
+      link.href = url
+      link.dataset.turboMethod = method
+      document.body.appendChild(link)
+      link.click()
+      document.body.removeChild(link)
+    }
     this.close()
   }
 }
