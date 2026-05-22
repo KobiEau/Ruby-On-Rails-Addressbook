5dbf15238c4d2621a8ff2e0e4c2b4db1f0c4bdde
KobiEau <nana.enyan-aidoo@st.vvu.edu.gh>
2026-05-22 22:19:32 +0000

    fix: fix typos, update 'after_update_path_for' method

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,4 @@
-class Users::RegistrationController < Devise::RegistrationsController
+class Users::RegistrationsController < Devise::RegistrationsController
   #handling  password change separately other fields
   def update
     if params[:user][:current_password].present?
@@ -8,27 +8,32 @@ class Users::RegistrationController < Devise::RegistrationsController
       if @user.update_without_password(update_params)
         redirect_to edit_user_registration_path, notice: "Account Profile updated."
       else
-        render :edit, status: :unporcessable_entity
+        render :edit, status: :unprocessable_entity
       end
     end
   end 
-
-  private
-
-  def update_params
-    params.require(:user).permit(:email,:firstname,:lastname)
-  end
-
-  def sign_up_params
-    params.require(:user).permit.(:email, :password, :password_confirmation,:firstname,:lastname)
-  end
-
+  
   def after_sign_up_path_for(resource)
     #where to go after signing up - contacts index
     contacts_path
   end
 
   def after_update_path_for(resource)
-      #stay on edit page after updating profile
+    edit_user_registration_path
   end
+
+  private
+
+  def update_params
+    params.require(:user).permit(:firstname, :lastname)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:email, :firstname, :lastname, :current_password, :password, :password_confirmation)
+  end
+
+  def sign_up_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :firstname, :lastname)
+  end
+
 end
