4837609d4fe4d1f191a7135e5701dcb8dd05cfdd
KobiEau <nana.enyan-aidoo@st.vvu.edu.gh>
2026-05-22 16:06:50 +0000

    fix: type correct method name

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,9 +47,9 @@ Rails.application.configure do
   # config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
 
   # letter_opener
-  config.action_mailer.delivery method = :letter_opener
+  config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: "localhost",port:3000}
+  #config.action_mailer.default_url_options = { host: "localhost",port:3000}
   
   # Make template changes take effect immediately.
   config.action_mailer.perform_caching = false
