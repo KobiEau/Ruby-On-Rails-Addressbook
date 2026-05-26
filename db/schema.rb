6d84b8d4abb7f799de99b7ad1c89bed7228b31de
KobiEau <nana.enyan-aidoo@st.vvu.edu.gh>
2026-05-26 08:18:54 +0000

    feat(auth): add confirmation fields to users table for email verification

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_20_105202) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_25_105918) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -42,6 +42,9 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_20_105202) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.datetime "confirmation_sent_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
     t.datetime "created_at", null: false
     t.datetime "current_sign_in_at"
     t.string "current_sign_in_ip"
@@ -58,8 +61,10 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_20_105202) do
     t.string "reset_password_token"
     t.string "role_code", default: "usr", null: false
     t.integer "sign_in_count", default: 0
+    t.string "unconfirmed_email"
     t.string "unlock_token"
     t.datetime "updated_at", null: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["role_code"], name: "index_users_on_role_code"
