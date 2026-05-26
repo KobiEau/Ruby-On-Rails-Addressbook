a8e581832029becbc49677d2f3b6f5271c33df2b
KobiEau <nana.enyan-aidoo@st.vvu.edu.gh>
2026-05-26 16:23:59 +0000

    refactor(auth): enhance user authentication flow and update account management methods

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,26 +1,43 @@
 class AccountsController < ApplicationController
-  before_action :require_authentication
-  before_action :set_user, only: %i[show edit update edit_password update_password destroy]
+  before_action :authenticate_user!
+  before_action :set_user, only: %i[show edit update edit_email update_email edit_password update_password danger destroy]
+ 
   def show
   end
 
   def edit
   end
-
+  
   def update
-    if @user.update(user_params)
-      redirect_to account_path, notice: "Account updated"
-    else
-      render :edit,status: :unprocessable_entity
-    end
+     if @user.update_without_password(profile_params)
+      redirect_to account_path, notice: "Profile updated"
+     else
+      render :edit, status: :unprocessable_entity
+     end
+  end
+  
+  def edit_email
   end
 
+  def update_email
+    unless @user.valid_password?(params[:user][:current_password])
+      flash.now[:alert] = "Current password incorrect"
+      return render :edit_email, status: :unprocessable_entity
+    end
+
+    if @user.update(email_params)
+      redirect_to account_path, notice: "Email updated"
+    else
+      render :edit_email, status: :unprocessable_entity
+    end
+  end
+  
   def edit_password
    
   end
 
   def update_password
-    unless @user.authenticate(params[:current_password])
+    unless @user.valid_password?(params[:user][:current_password])
       flash.now[:alert] = "Current password is incorrect"
       return render :edit_password, status: :unprocessable_entity
     end
@@ -28,19 +45,23 @@ class AccountsController < ApplicationController
     if @user.update(password_params)
       redirect_to account_path, notice: "Password updated successfully"
     else
-      flash.now[:alert] = "Incorrect credentials"
       render :edit_password, status: :unprocessable_entity
     end
   end
 
+
+  def danger
+        
+  end
+
   def destroy
-    unless @user.authenticate(params[:current_password])
+    unless @user.valid_password?(params[:user][:current_password])
       flash[:alert] = "Incorrect Password"
-      return redirect_to edit_account_path
+      return redirect_to danger_account_path
     end
 
+    sign_out(@user)
     @user.destroy
-    reset_session
     redirect_to root_path, notice: "Account deleted"
   end
 
@@ -49,11 +70,15 @@ class AccountsController < ApplicationController
     @user = current_user
   end
   
-  def user_params
-    params.require(:user).permit(:firstname, :lastname, :email_address)
+  def email_params
+    params.require(:user).permit(:email)
   end
 
   def password_params
-    params.require(:user).permit(:password,:password_confirmation)
+    params.require(:user).permit(:password, :password_confirmation)
+  end
+
+  def profile_params
+    params.require(:user).permit(:firstname, :lastname)
   end
 end
