f45205fc3d59aeef8fc5bf2829ac4daa5f15d0b4
KobiEau <nana.enyan-aidoo@st.vvu.edu.gh>
2026-05-31 23:10:18 +0000

    feat: add 2 statistics to dashboard controller

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -4,5 +4,7 @@ class Admin::DashboardController < Admin::BaseController
     @total_contacts = Contact.count
     @total_admins = User.where(role_code: Role::ADMIN).count
     @recent_users = User.includes(:role,:contacts).order(created_at: :desc).limit(5)
+    @contacts_this_week = Contact.where(created_at: 1.week.ago..Time.current).count
+    @errors_today = ErrorLog.where(created_at: Date.today.all_day).count
   end
 end
