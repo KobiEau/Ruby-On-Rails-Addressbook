d579109535135ceeaabb350fd74dc0feaab7f85a
KobiEau <nana.enyan-aidoo@st.vvu.edu.gh>
2026-06-01 00:41:34 +0000

    feat: add error logs routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
  
   namespace :admin do
     root "dashboard#index"
+    resources :error_logs, only: [:index, :destroy]
     resources :users do
       collection do
         delete :bulk_destroy
