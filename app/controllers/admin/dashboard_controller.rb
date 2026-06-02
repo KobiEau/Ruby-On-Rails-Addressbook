3bc3f33f58a488eb10a56ace3b6f0e8fa6798f56
KobiEau <nana.enyan-aidoo@st.vvu.edu.gh>
2026-06-02 13:16:54 +0000

    feat: count only users with '@total_users',no need to count admins

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class Admin::DashboardController < Admin::BaseController
   def index
-    @total_users = User.count
+    @total_users = User.where(role_code: Role::USER).count
     @total_contacts = Contact.count
     @total_admins = User.where(role_code: Role::ADMIN).count
     @recent_users = User.includes(:role,:contacts).order(created_at: :desc).limit(5)
