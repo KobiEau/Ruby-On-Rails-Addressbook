55055b27f26d7f11218bd6e5831ef7dce23675a1
KobiEau <nana.enyan-aidoo@st.vvu.edu.gh>
2026-06-02 15:10:48 +0000

    feat(db): add backtrace column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_29_124501) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_01_004908) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -27,6 +27,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_29_124501) do
   end
 
   create_table "error_logs", force: :cascade do |t|
+    t.text "backtrace", default: [], array: true
     t.datetime "created_at", null: false
     t.string "error_class", null: false
     t.string "http_method"
