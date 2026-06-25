e2956b35e4518a309b1c9ad06ceab13d4ef3c206
KobiEau <nana.enyan-aidoo@st.vvu.edu.gh>
2026-06-25 00:24:13 +0000

    feat: add seeds for creating users and contacts

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,54 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-Role.find_or_create_by(code:"usr") {|r| r.name ="User"}
-Role.find_or_create_by(code:"adm") {|r| r.name ="Administrator"}
+def creating(category)
+  puts "creating #{category}"
+end
+
+puts "Creating roles"
+user_role = Role.find_or_create_by(code:"usr") {|r| r.name ="User"}
+admin_role = Role.find_or_create_by(code:"adm") {|r| r.name ="Administrator"}
 
 puts "Roles seeded:"
 Role.all.each {|r| puts "#{r.code}- #{r.name}"}
+
+
+# Seeding admin
+puts "Creating admin"
+admin = User.find_or_create_by!(
+  email: "admin@example.com"
+)do |user|
+  user.firstname = "System"
+  user.lastname = "Admin"
+  user.password = "password123"
+  user.password_confirmation = "password123"
+  user.role = admin_role
+end
+
+# regular user (client)
+
+5.times do |i|
+  User.find_or_create_by!(
+    email: "user#{i+1}@exmaple.com"
+  ) do |user|
+    user.firstname = "user"
+    user.lastname = "#{i+1}"
+    user.password = "user_password_#{i+1}"
+    user.password_confirmation = "userpassword#{i+1}"
+    user.role = user_role
+  end
+end
+
+#Contacts
+User.where(role: user_role).find_each do |user|
+  5.times do |i|
+    Contact.find_or_create_by!(
+      user: user,
+      phone_number: "024000000#{i}"
+    )do |contact|
+      contact.firstname = "Contact"
+      contact.lastname = "#{i+1}"
+      contact.category = "Uncategorised"
+    end
+  end
+end
